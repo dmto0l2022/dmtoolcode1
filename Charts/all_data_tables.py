68b5c30207f2c051b02fb4e94ca08930bbb07249
andrewcgaitskell <andrew@gaitskell.com>
2022-10-14 15:52:42 +0100

    new more compact dashboard in progress

--- Charts/all_data_tables.py
+++ Charts/all_data_tables.py
@@ -76,6 +76,10 @@ years_df = pd.DataFrame(data=years,columns=['year'])
 
 ###########################################################################
 
+table_heights = 120
+
+style_header_var={ 'backgroundColor': 'black','color': 'white'}
+
 years_table = dash_table.DataTable(
     id='years_table',
     columns=[
@@ -85,7 +89,7 @@ years_table = dash_table.DataTable(
     filter_action='none',
     row_selectable='multi',
     fixed_rows={'headers': True},
-    #page_size=5,
+    #page_size=3,
     style_cell_conditional=[
         {'if': {'column_id': 'year'},
          'width': '90%'},
@@ -104,8 +108,9 @@ years_table = dash_table.DataTable(
     }],
     selected_rows=[],
     style_table={
-        'height': 250,
+        'height': table_heights,
     },
+    style_header=style_header_var,
     #style_data={
     #    'width': '25px', 'minWidth': '25px', 'maxWidth': '25px',
     #    ##'overflow': 'hidden',
@@ -141,8 +146,9 @@ limit_types_table = dash_table.DataTable(
     }],
     selected_rows=[],
     style_table={
-        'height': 250,
-    }
+        'height': table_heights,
+    },
+    style_header=style_header_var,
     #style_data={
      #   'width': '25px', 'minWidth': '25px', 'maxWidth': '25px',
      #   ##'overflow': 'hidden',
@@ -164,7 +170,7 @@ experiments_table = dash_table.DataTable(
          'width': '90%'},
     ],
     fixed_rows={'headers': True},
-    style_table={'height': 250},  # defaults to 500
+    style_table={'height': table_heights},  # defaults to 500
     style_cell={'textAlign': 'left','padding': '0px','font_size': '12px',
             'overflow': 'hidden',
             'textOverflow': 'ellipsis',
@@ -177,7 +183,8 @@ experiments_table = dash_table.DataTable(
             overflow-y: hidden;
         '''
     }],
-    selected_rows=[]
+    selected_rows=[],
+    style_header=style_header_var,
     #style_data={
     #    'width': '25px', 'minWidth': '25px', 'maxWidth': '25px',
     #    ##'overflow': 'hidden',
@@ -213,8 +220,9 @@ result_types_table = dash_table.DataTable(
     fixed_rows={'headers': True},
     selected_rows=[],
     style_table={
-        'height': 250,
-    }#,
+        'height': table_heights,
+    },
+    style_header=style_header_var,
     #style_data={
     #    'width': '25px', 'minWidth': '25px', 'maxWidth': '25px',
     #    ##'overflow': 'hidden',
@@ -250,8 +258,9 @@ spin_dependency_table = dash_table.DataTable(
     fixed_rows={'headers': True},
     selected_rows=[],
     style_table={
-        'height': 250,
-    }
+        'height': table_heights,
+    },
+    style_header=style_header_var,
     #style_data={
     #    'width': '25px', 'minWidth': '25px', 'maxWidth': '25px',
     #    ##'overflow': 'hidden',
@@ -277,6 +286,7 @@ greatest_hit_table = dash_table.DataTable(
     style_cell={'textAlign': 'left','padding': '0px','font_size': '12px',
             'overflow': 'hidden',
             'textOverflow': 'ellipsis',
+                'border': '1px solid black'
         },
     css=[{
         'selector': '.dash-spreadsheet td div',
@@ -286,7 +296,8 @@ greatest_hit_table = dash_table.DataTable(
             overflow-y: hidden;
         '''
     }],
-    style_table={'height': 250,}
+    style_table={'height': table_heights,},
+    style_header=style_header_var,
     #style_data={
     #    'width': '25px', 'minWidth': '25px', 'maxWidth': '25px',
         ##'overflow': 'hidden',
@@ -300,14 +311,14 @@ limits_table = dash_table.DataTable(
     columns=[{'name': 'expid', 'id': 'expid'},
              {'name': 'data_reference', 'id': 'data_reference'},
              {'name': 'data_label', 'id': 'data_label'},
-             {'name': 'experiment', 'id': 'experiment'},
+             #{'name': 'experiment', 'id': 'experiment'},
              #{'name': 'spin_dependency', 'id': 'spin_dependency'},
-             {'name': 'result_type', 'id': 'result_type'},
-             {'name': 'year', 'id': 'year'},
+             #{'name': 'result_type', 'id': 'result_type'},
+             #{'name': 'year', 'id': 'year'},
              ],
     #fixed_rows={'headers': True},
     page_size=5,
-    filter_action='native',
+    filter_action='none',
     #row_selectable='multi',
     #selected_rows=[],
     style_cell={'textAlign': 'left','padding': '0px','font_size': '12px',
@@ -323,20 +334,20 @@ limits_table = dash_table.DataTable(
             overflow-y: hidden;
         '''
     }],
-    style_table={'height': 320,},
+    style_table={'height': '25vh',},
     style_cell_conditional=[
         {'if': {'column_id': 'expid'},
          'width': '5%'},
         {'if': {'column_id': 'data_reference'},
-         'width': '45%'},
+         'width': '20%'},
         {'if': {'column_id': 'data_label'},
-         'width': '45%'},
+         'width': '35%'},
     ],
     style_data={
         'whiteSpace': 'normal',
         'height': 'auto',
     },
-    
+    style_header=style_header_var,
     #tooltip_data=[
     #    {
     #        column: {'value': str(value), 'type': 'markdown'}
@@ -379,23 +390,24 @@ plots_table = dash_table.DataTable(
     #sort_mode='multi',
     #sort_as_null=['', 'No'],
     #sort_by=[{'column_id': 'expid', 'direction': 'desc'}],
-    filter_action='native',
+    filter_action='none',
     row_deletable=True,
     #row_selectable='multi',
     #selected_rows=[],
-    style_table={'height': 200,},
+    style_table={'height': '25vh',},
     style_cell_conditional=[
         {'if': {'column_id': 'expid'},
          'width': '5%'},
         {'if': {'column_id': 'tablerowid'},
          'width': '5%'},
         {'if': {'column_id': 'data_reference'},
-         'width': '40%'},
+         'width': '20%'},
         {'if': {'column_id': 'data_label'},
-         'width': '45%'},
+         'width': '35%'},
     ],
     style_data={
         'whiteSpace': 'normal',
         'height': 'auto',
     },
+    style_header=style_header_var,
 )
