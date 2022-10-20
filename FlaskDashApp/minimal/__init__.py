306d6c6226a30538431e3bad051346e8075c4de1
andrewcgaitskell <andrew@gaitskell.com>
2022-10-20 15:05:33 +0100

    Create __init__.py

---
+++ FlaskDashApp/minimal/__init__.py
@@ -0,0 +1,14 @@
+"""Initialize Flask app."""
+from flask import Flask
+
+def init_app():
+    """Construct core Flask application with embedded Dash app."""
+    app = Flask(__name__, instance_relative_config=False)
+    #app.config.from_object('config.Config')
+
+    with app.app_context():
+        # Import Dash application
+        from minimal import create_dashboard
+        app = create_dashboard(app)
+
+        return app
