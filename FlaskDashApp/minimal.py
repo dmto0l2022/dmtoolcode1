282a86c115d1e0325156a7bda6978d8a4f91dc64
andrewcgaitskell <andrew@gaitskell.com>
2022-10-20 14:10:33 +0100

    Create minimal.py

---
+++ FlaskDashApp/minimal.py
@@ -0,0 +1,16 @@
+from dash import Dash
+from dash import dcc
+from dash import html
+
+app = Dash(
+    __name__,
+    external_stylesheets=['/static/dist/css/style.css'],
+    external_scripts=external_scripts,
+    routes_pathname_prefix='/dash/'
+)
+
+app.layout = html.Div(id='example-div-element')
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
