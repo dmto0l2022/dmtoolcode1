cf8afbb55ad9261450c885cccf308a52f240891c
andrewcgaitskell <andrew@gaitskell.com>
2022-10-20 14:51:08 +0100

    Update minimal.py

--- FlaskDashApp/minimal.py
+++ FlaskDashApp/minimal.py
@@ -5,11 +5,11 @@ from dash import html
 app = Dash(
     __name__,
     external_stylesheets=['/static/style.css'],
-    external_scripts=external_scripts,
+    ##external_scripts=external_scripts,
     routes_pathname_prefix='/dash/'
 )
 
-app.layout = html.Div(id='example-div-element')
+app.layout = html.Div('minimal dash app', id='example-div-element')
 
 
 if __name__ == '__main__':
