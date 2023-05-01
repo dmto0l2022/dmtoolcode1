6c7f5854535d744e1436d85a3b0b7e9242ffa2f5
andrewcgaitskell <andrew@gaitskell.com>
2023-05-01 12:08:17 +0100

    almost all form row components created

--- Charts/forms/formlibrary.py
+++ Charts/forms/formlibrary.py
@@ -4,6 +4,80 @@ from dash import html
 import dash_bootstrap_components as dbc
 
 
+######################### fields ######################
+
+
+# Plot Name - Text
+
+# X Range: Lower Bound - Text
+
+# Upper Bound - Text
+
+# Scale - Dropdown
+
+# Trace Trace ID - Integer
+
+# Trace Name - Text
+
+# Trace Color - Dropdown Trace
+
+# Symbol - Dropdown
+
+# Trace Line Style - Dropdown
+
+# Trace Fill Color - Dropdown
+
+# Remove Site Address Check Box
+
+# Upload XML File - Select File
+
+# Load Limit from Uploaded File
+
+# Help on XML File and Examples
+
+# Data Label - Text - < 60 chars
+
+# Data Reference - Big Text
+
+# Data Comment - Big Text
+
+# Experiment - Dropdown
+
+# Date of Announcement - Date
+
+# Year - Dropdown - Integer
+
+# Y Rescale - Exponential
+
+# X Rescale - Exponential
+
+# Data Values - big text field
+
+# Data Formatting Help
+
+# Result Type - Dropdown
+
+# Spin Dependency - Dropdown
+
+# Measurement Type -
+
+# Public Limit - Checkbox
+
+# Other Users - CSV of Users
+
+# Official - Dropdown
+
+# Experiment Type - Dropdown
+
+# Year - Dropdown
+
+# Greatest Hits - Dropdown
+
+
+
+##########################
+
+
 ################################################
 ## NEW PLOT FORM
 ################################################
